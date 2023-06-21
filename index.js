80ae80902c1bbc5be184f6c366209b60ab422106
DoubleG254 <glorygweth6@gmail.com>
2023-06-21 10:13:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+const dodger = document.getElementById ("dodger");
+function moveDodgerLeft(){
+    const leftnumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftnumbers, 10);
+
+    if(left>0){
+        dodger.style.left=`${left -1}px`;
+    }
+}
+
+function moveDodgerRight(){
+    const leftnumber= dodger.style.left.replace("px","");
+    const left= parseInt(leftnumber, 10);
+
+    if (left<360) {
+        dodger.style.left=`${left+1}px`
+    }
+}
+document.addEventListener("keydown",function(e){
+    if(e==="ArrowLeft"){
+        moveDodgerLeft();
+    }
+    else if (e==="ArrowRight") {
+        moveDodgerRight();
+    }
+ }
+)
